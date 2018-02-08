41813b02cb3fa5ac738eba67b55b0133e8717bbf
CincoSwim <35818836+CincoSwim@users.noreply.github.com>
2018-02-08 15:09:21 -0500

    First iteration of an uppercase character converter.

--- src/ConvertToUpper.java
+++ src/ConvertToUpper.java
@@ -1,2 +1,21 @@
+import java.util.*;
+
 public class ConvertToUpper {
+    public static void main(String[] args) {
+        Scanner keyin = new Scanner(System.in);
+
+        String lower;
+        String upper;
+
+        System.out.print("Enter a letter: ");
+        lower = keyin.next();
+
+        if (lower.length() == 1 && lower.matches("[a-z]")) {
+            upper = lower.toUpperCase();
+            System.out.println("The uppercase equivalent of " + lower + " is " + upper);
+        } else if (lower.length() != 1)
+            System.out.println("You have entered an incorrect number of characters");
+        else
+            System.out.println("Not a letter");
+    }
 }
