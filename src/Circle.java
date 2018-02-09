c85df3e7c2df09d4ec8e1cd6b658149f6db4cc05
CincoSwim <cgpucko@gmail.com>
2018-02-09 17:28:45 -0500

    Did some badass things. First little bit of gui with JOptionPane. I know it sounds noobish but this is pretty cool. Checks if a coordinate point is within a 10 unit radius circle.

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,52 @@
+import java.util.*;
+import javax.swing.*;
+
 public class Circle {
+    static double x;
+    static double y;
+    static String input;
+    static double leng;
+
+    public static class InputPane {
+        JFrame f;
+        InputPane(){
+            f = new JFrame();
+
+            input = JOptionPane.showInputDialog(f,"Enter X Coordinate:");
+            x = Double.parseDouble(input);
+
+            input = JOptionPane.showInputDialog(f,"Enter Y Coordinate:");
+            y = Double.parseDouble(input);
+
+        }
+
+    }
+    public static class OutputTestTrue {
+        JFrame f;
+        OutputTestTrue(){
+            f = new JFrame();
+            JOptionPane.showMessageDialog(f,"Point (" + (int) x + ", " + (int) y + ") is in the circle.","Hooray!", JOptionPane.WARNING_MESSAGE);
+
+        }
+    }
+    public static class OutputTestFalse {
+        JFrame f;
+        OutputTestFalse(){
+            f = new JFrame();
+            f = new JFrame();
+            JOptionPane.showMessageDialog(f,"Point (" + (int) x + ", " + (int) y + ") is not in the circle.","Alert!", JOptionPane.WARNING_MESSAGE);
+
+        }
+    }
+    public static void main (String[] args){
+       new InputPane();
+
+       leng = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+
+       if (leng < 10.0)
+           new OutputTestTrue();
+       else
+           new OutputTestFalse();
+
+    }
 }
