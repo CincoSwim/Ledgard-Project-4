735222120ad87a7ec1c0829133d4dac8c8782cf9
CincoSwim <35818836+CincoSwim@users.noreply.github.com>
2018-02-10 21:39:26 -0500

    did a password checker with 3 tries. made it as a string, just so it doesn't throw InputMismatch.

--- src/ThreeGuesses.java
+++ src/ThreeGuesses.java
@@ -1,2 +1,36 @@
+import java.util.*;
+
 public class ThreeGuesses {
+    public static void main(String[] args) {
+        String guesses;
+        String guess;
+        String secretPass;
+
+        Scanner input = new Scanner(System.in);
+        secretPass = "28";
+
+        System.out.println("Welcome to the People's Bank.");
+        System.out.print("Please enter your secret code:");
+        guess = input.next();
+
+        if (guess.equals(secretPass))
+            System.out.println("Fine, go ahead.");
+        else {
+            System.out.print("Sorry, that is not it. Try again:");
+            guess = input.next();
+
+            if (guess.equals(secretPass))
+                System.out.println("Fine, go ahead.");
+            else {
+                System.out.print("Sorry, last chance. Try again:");
+                guess = input.next();
+
+                if (guess.equals(secretPass))
+                    System.out.println("Fine, go ahead.");
+                else {
+                    System.out.println("Sorry, that's too many guesses. Please try again tomorrow.");
+                }
+            }
+        }
+    }
 }
